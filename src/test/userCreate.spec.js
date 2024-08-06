f0bdd7782ebb167b06dd386a681ea6508ca6a943
Alfira Galliamova <alfiragalliamova@mbp-alfira.mynetworksettings.com>
2024-08-05 23:18:51 -0400

    first test

---
+++ src/test/userCreate.spec.js
@@ -0,0 +1,30 @@
+const request = require('supertest')
+const { expect } = require('chai')
+describe('USER CREATE', () => {
+    describe('USER CREATE - POSITIVE TEST', () => {
+        it('user create all fields', () => {
+
+
+
+        })
+    })
+
+    describe('USER CREATE - NEGATIVE TEST', () => {
+        it('user create all fields', () => {
+
+        })
+        it('user create all fields', () => {
+        
+        
+        })
+        it('user create all fields', () => {
+
+        })
+        it('user create all fields', () => {
+
+
+            
+        })
+    })
+})
+
