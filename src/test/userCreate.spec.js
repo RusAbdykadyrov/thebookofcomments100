9f20155c90b8b95e5c30d03a099e4dc28441985f
Alfira Galliamova <alfiragalliamova@mbp-alfira.mynetworksettings.com>
2024-08-06 23:19:47 -0400

    userCreate.spec.js has been added

--- src/test/userCreate.spec.js
+++ src/test/userCreate.spec.js
@@ -1,12 +1,35 @@
-const request = require('supertest')
+
 const { expect } = require('chai')
+const {userCreateQ} = require('./queries')
+const{user} = require('./data')
+const gqRequest = require('../gqRequest')
+
+
+
+let respData = null
+let postData = null
+
 describe('USER CREATE', () => {
     describe('USER CREATE - POSITIVE TEST', () => {
         it('user create all fields', () => {
+            postData ={
+                query: userCreateQ,
+                variables: user
+            }
+            gqRequest(postData)
+                .expect(200)
+                .end((err, res) => {
+                    if (err) return done(err)
+                    respData = res.body.data
+                   // expect(respData).eq()
+                    console.log(respData)
+                    done()
 
-
+                })
 
         })
+        it('user create all fields', () => {
+    })
     })
 
     describe('USER CREATE - NEGATIVE TEST', () => {
